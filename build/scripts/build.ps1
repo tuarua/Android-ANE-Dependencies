8f13aabfe5cdb88cb1b78ed78993e92c9e81b6bf
Eoin Landy <el111@yahoo.com>
2018-06-17 16:23:57 +0100

    Firebase invites

--- build/scripts/build.ps1
+++ build/scripts/build.ps1
@@ -3,6 +3,73 @@ Param(
 [string] $package
 )
 
+#################### Cmdlets ##################
+function Get-Package {
+    param( [string]$groupId, [string]$artifactId, [string]$version, [string]$type, [string]$repo, [string]$category )
+    Write-Host "Getting package for $artifactId" -ForegroundColor yellow
+    $groupIdPath = $groupId -replace "\.", "/"
+
+    $packageDirectory = "$currentDir\cache\$category\$groupId"
+    $packageName = "$artifactId-$version"
+
+    if (-not (Test-Path $packageDirectory\$groupId-$packageName.jar)) {
+        if ($repo -eq "maven") {
+            $repoUri = $MAVEN_REPO
+        }elseif ($repo -eq "fabric") {
+            $repoUri = $FABRIC_REPO
+        }elseif ($repo -eq "google") {
+            $repoUri = $GOOGLE_REPO
+        }elseif ($repo -eq "jcenter") {
+            $repoUri = $JCENTER_REPO
+        }
+
+        if (-not (Test-Path $packageDirectory)) {
+            New-Item -ItemType Directory -Force -Path $packageDirectory
+        }
+        
+        Write-Host "Downloading $repoUri$groupIdPath/$artifactId/$version/$packageName.$type -OutFile $packageDirectory\$packageName.$type" -ForegroundColor yellow
+
+        Invoke-WebRequest -Uri $repoUri$groupIdPath/$artifactId/$version/$packageName.$type -OutFile $packageDirectory\$packageName.$type
+
+        if ($type -eq "aar") {
+            Rename-Item -NewName $packageDirectory\$packageName.zip -Path $packageDirectory\$packageName.aar
+            Expand-Archive -Path $packageDirectory\$packageName.zip -DestinationPath $packageDirectory\$packageName
+            Remove-Item -Path $packageDirectory\$packageName.zip
+            Rename-Item -NewName $packageDirectory\$packageName\$packageName.jar -Path $packageDirectory\$packageName\classes.jar
+            Move-Item -Path $packageDirectory\$packageName\$packageName.jar -Destination $packageDirectory\$packageName.jar
+            
+            if ((Test-Path $currentDir\cache\$category\$groupId-$packageName-res)) {
+                Remove-Item -Path $currentDir\cache\$category\$groupId-$packageName-res -Recurse
+            }
+
+            if ((Test-Path $packageDirectory\$packageName\res)) {
+                Move-Item -Path $packageDirectory\$packageName\res -Destination $currentDir\cache\$category\$groupId-$packageName-res
+            }
+            
+            Remove-Item -Path $packageDirectory\$packageName -Recurse
+        } 
+
+        Rename-Item -NewName $packageDirectory\$groupId-$packageName.jar -Path  $packageDirectory\$packageName.jar
+        Move-Item -Path $packageDirectory\$groupId-$packageName.jar -Destination $currentDir\cache\$category\$groupId-$packageName.jar -Force ## TODO might already exist
+        Remove-Item -Path $packageDirectory -Recurse
+
+    }
+}
+
+function GetPackagedResourceXML {
+    param( [string]$groupId, [string]$artifactId, [string]$version, [string]$packageName )
+    $ret = "<packagedResource>
+            <packageName>$packageName</packageName>
+            <folderName>$groupId-$artifactId-$version-res</folderName>
+        </packagedResource>"
+
+    ## Write-Host "XML $ret" -ForegroundColor green
+
+    return $ret
+}
+
+###############################################
+
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
 $AIR_SDK = Get-Content "$currentDir\airsdk.config"
 $ADT_PATH = "$AIR_SDK\bin\adt.bat"
@@ -17,51 +84,10 @@ if (-not (Test-Path $ADT_PATH)) {
     Exit;
 }
 
-Write-Host "Using AIR SDK $ADT_PATH" -ForegroundColor yellow
-
-
 if (-not (Test-Path $currentDir\cache)) {
     New-Item -Path $currentDir\cache
 }
 
-
-function Get-Package {
-    param( [string]$groupId, [string]$artifactId, [string]$version, [string]$type, [string]$repo, [string]$category )
-    Write-Host "Getting package for $artifactId" -ForegroundColor yellow
-    $groupIdPath = $groupId -replace "\.", "/"
-    if (-not (Test-Path $currentDir\cache\$category\$artifactId-$version.jar)) {
-        if ($repo -eq "maven") {
-            $repoUri = $MAVEN_REPO
-        }elseif ($repo -eq "fabric") {
-            $repoUri = $FABRIC_REPO
-        }elseif ($repo -eq "google") {
-            $repoUri = $GOOGLE_REPO
-        }elseif ($repo -eq "jcenter") {
-            $repoUri = $JCENTER_REPO
-        }
-
-        Write-Host "Downloading $repoUri$groupIdPath/$artifactId/$version/$artifactId-$version.$type -OutFile $currentDir\cache\$category\$artifactId-$version.$type" -ForegroundColor yellow
-
-        Invoke-WebRequest -Uri $repoUri$groupIdPath/$artifactId/$version/$artifactId-$version.$type -OutFile $currentDir\cache\$category\$artifactId-$version.$type
-
-        if ($type -eq "aar") {
-            Rename-Item -NewName $currentDir\cache\$category\$artifactId-$version.zip -Path $currentDir\cache\$category\$artifactId-$version.aar
-            Expand-Archive -Path $currentDir\cache\$category\$artifactId-$version.zip -DestinationPath $currentDir\cache\$category\$artifactId-$version
-            Remove-Item -Path $currentDir\cache\$category\$artifactId-$version.zip
-            if ((Test-Path $currentDir\cache\$category\$artifactId-$version\res)) {
-                Rename-Item -NewName $currentDir\cache\$category\$artifactId-$version\$artifactId-$version-res -Path $currentDir\cache\$category\$artifactId-$version\res
-            }
-            Rename-Item -NewName $currentDir\cache\$category\$artifactId-$version\$artifactId-$version.jar -Path $currentDir\cache\$category\$artifactId-$version\classes.jar
-            if ((Test-Path $currentDir\cache\$category\$artifactId-$version\$artifactId-$version-res)) {
-                Move-Item -Path $currentDir\cache\$category\$artifactId-$version\$artifactId-$version-res -Destination $currentDir\cache\$category\$artifactId-$version-res
-            }
-            Move-Item -Path $currentDir\cache\$category\$artifactId-$version\$artifactId-$version.jar -Destination $currentDir\cache\$category\$artifactId-$version.jar
-            Remove-Item -Path $currentDir\cache\$category\$artifactId-$version -Recurse
-        }
-
-    }
-
-}
 
 [xml]$XmlDocument = Get-Content $currentDir\packages.xml
 
@@ -88,27 +114,27 @@ for($i=0;$i -lt $XmlDocument.packages.ChildNodes.Count;$i++) {
     if (-not (Test-Path $currentDir\cache\$category)) {
         New-Item -Path $currentDir\cache\$category -ItemType "directory"
     }
-
-
-    Get-Package $groupId $artifactId $version $type $repo $category
     
+    Get-Package $groupId $artifactId $version $type $repo $category
+
     $packagedResources = ""
     $packagedResourceLoops = ""
     $packagedDependencies = ""
     $packagedDependencyLoops = ""
-    $packagedDependencyLoops = "<packagedDependency>$artifactId-$version.jar</packagedDependency>"
+    $packagedDependencyLoops = "<packagedDependency>$groupId-$artifactId-$version.jar</packagedDependency>" ##TODO jar here
     
     $numDependancies = $XmlDocument.packages.package[$i].dependancies.ChildNodes.Count
     $numResources = $numDependancies
 
     if ($type -eq "aar") {
         $numResources = $numResources + 1
-        $packagedResourceLoops += "
-        <packagedResource>
-            <packageName>$packageName</packageName>
-            <folderName>$artifactId-$version-res</folderName>
-        </packagedResource>"
+        $packagedResourceLoops += GetPackagedResourceXML $groupId $artifactId $version $packageName
     }
+
+    ## Write-Host "XML $packagedResourceLoops" -ForegroundColor green
+    
+
+    ## depend PUT BACK
 
     if($numDependancies -gt 0) {
         $dependancies = $XmlDocument.packages.package[$i].dependancies
@@ -119,32 +145,44 @@ for($i=0;$i -lt $XmlDocument.packages.ChildNodes.Count;$i++) {
                 $depend_version = $dependancies.package.version
                 $depend_type = $dependancies.package.type
                 $depend_repo = $dependancies.package.repo
+                $depend_packageName = "$depend_groupId.$depend_artifactId"
+
+                if($dependancies.package.packageName) {
+                    $depend_packageName = $dependancies.package.packageName
+                }
+
             } else {
                 $depend_groupId = $dependancies.package[$j].groupId
                 $depend_artifactId = $dependancies.package[$j].artifactId
                 $depend_version = $dependancies.package[$j].version
                 $depend_type = $dependancies.package[$j].type
                 $depend_repo = $dependancies.package[$j].repo
+                $depend_packageName = "$depend_groupId.$depend_artifactId"
+
+
+                if($dependancies.package[$j].packageName) {
+                    $depend_packageName = $dependancies.package[$j].packageName
+                }
             }
-            $depend_packageName = "$depend_groupId.$depend_artifactId"
+            
             
             Get-Package $depend_groupId $depend_artifactId $depend_version $depend_type $depend_repo $category
 
-            if($dependancies.package[$j].packageName) {
-                $depend_packageName = $dependancies.package[$j].packageName
-            }
+            
 
-            $packagedDependencyLoops += "<packagedDependency>$depend_artifactId-$depend_version.jar</packagedDependency>"
+            Write-Host ---------------------------------------" -ForegroundColor green
+            Write-Host "depend_packageName $depend_packageName" -ForegroundColor green
+            Write-Host ---------------------------------------" -ForegroundColor green
+
+
+            $packagedDependencyLoops += "<packagedDependency>$depend_groupId-$depend_artifactId-$depend_version.jar</packagedDependency>"
             if ($depend_type -eq "aar") {
-            $packagedResourceLoops += "
-            <packagedResource>
-            <packageName>$depend_packageName</packageName>
-            <folderName>$depend_artifactId-$depend_version-res</folderName>
-            </packagedResource>"
+                $packagedResourceLoops += GetPackagedResourceXML $depend_groupId $depend_artifactId $depend_version $depend_packageName
             }
 
         }
     }
+    
 
     $packagedDependencies = "<packagedDependencies>
         $packagedDependencyLoops
@@ -153,7 +191,7 @@ for($i=0;$i -lt $XmlDocument.packages.ChildNodes.Count;$i++) {
     if($numResources -gt 0) {
          $packagedResources = "<packagedResources>
             $packagedResourceLoops
-            </packagedResources>"
+        </packagedResources>"
     }
 
     $platformXml = "<?xml version=`"1.0`" encoding=`"utf-8`"?>
@@ -190,7 +228,6 @@ for($i=0;$i -lt $XmlDocument.packages.ChildNodes.Count;$i++) {
     </platforms>
 </extension>"
     Set-Content -Path $currentDir\extension.xml -Value $extensionXml    
-
 
     ### Create Java Class ###
     $jPath = "$currentDir..\..\..\native_library\android\dummyane\src\main\java"
@@ -233,24 +270,25 @@ for($i=0;$i -lt $XmlDocument.packages.ChildNodes.Count;$i++) {
     Copy-Item -Path $currentDir\library.swf -Destination $currentDir\platforms\android -Force
     Copy-Item -Path "$currentDir\..\..\native_library\android\dummyane\build\libs\dummyane.jar" -Destination $currentDir\platforms\android\classes.jar -Force
     
+    $resFolderName = "$groupId-$artifactId-$version-res"
     
     if ($type -eq "aar") {
-        if ((Test-Path "$currentDir\cache\$category\$artifactId-$version-res")) {
-            Copy-Item -Path $currentDir\cache\$category\$artifactId-$version-res $currentDir\platforms\android -Force -Recurse
+        if ((Test-Path "$currentDir\cache\$category\$resFolderName")) {
+            Copy-Item -Path $currentDir\cache\$category\$resFolderName $currentDir\platforms\android -Force -Recurse
         }
 
-        Write-Host "copying res to $currentDir\platforms\android\$artifactId-$version-res" -ForegroundColor yellow
+        Write-Host "copying res to $currentDir\platforms\android\$resFolderName" -ForegroundColor yellow
 
-        if (-not (Test-Path "$currentDir\platforms\android\$artifactId-$version-res\values")) {
+        if (-not (Test-Path "$currentDir\platforms\android\$resFolderName\values")) {
 
-            Write-Host "need to write new strings $currentDir\platforms\android\$artifactId-$version-res\values\strings.xml" -ForegroundColor yellow
+            Write-Host "need to write new strings $currentDir\platforms\android\$resFolderName\values\strings.xml" -ForegroundColor yellow
 
-            New-Item -Path $currentDir\platforms\android\$artifactId-$version-res\values -ItemType "directory"
-            New-Item -Path $currentDir\platforms\android\$artifactId-$version-res\values\strings.xml
-            Set-Content -Path $currentDir\platforms\android\$artifactId-$version-res\values\strings.xml -Value $defaultResource
+            New-Item -Path $currentDir\platforms\android\$resFolderName\values -ItemType "directory"
+            New-Item -Path $currentDir\platforms\android\$resFolderName\values\strings.xml
+            Set-Content -Path $currentDir\platforms\android\$resFolderName\values\strings.xml -Value $defaultResource
         }
     }
-    Copy-Item -Path $currentDir\cache\$category\$artifactId-$version.jar $currentDir\platforms\android\$artifactId-$version.jar -Force
+    Copy-Item -Path $currentDir\cache\$category\$groupId-$artifactId-$version.jar $currentDir\platforms\android\$groupId-$artifactId-$version.jar -Force
 
     if (-not (Test-Path "$currentDir\..\..\anes\$category")) {
         New-Item -Path $currentDir\..\..\anes\$category -ItemType "directory"
@@ -264,9 +302,9 @@ for($i=0;$i -lt $XmlDocument.packages.ChildNodes.Count;$i++) {
     $ADT_FILES = ""
     $ADT_FILES += "-C platforms/android library.swf classes.jar "
     $ADT_FILES += "-platformoptions platforms/android/platform.xml "
-    $ADT_FILES += "$artifactId-$version.jar "
+    $ADT_FILES += "$groupId-$artifactId-$version.jar "
     if ($type -eq "aar") {
-        $ADT_FILES += "$artifactId-$version-res/. "
+        $ADT_FILES += "$resFolderName/. "
     }
 
 
@@ -287,25 +325,27 @@ for($i=0;$i -lt $XmlDocument.packages.ChildNodes.Count;$i++) {
                 $depend_repo = $dependancies.package[$j].repo
             }
 
+            $depend_resFolderName = "$depend_groupId-$depend_artifactId-$depend_version-res"
+
             if ($depend_type -eq "aar") {
-                if ((Test-Path $currentDir\cache\$category\$depend_artifactId-$depend_version-res)) {
-                    Copy-Item -Path $currentDir\cache\$category\$depend_artifactId-$depend_version-res $currentDir\platforms\android -Force -Recurse
+                if ((Test-Path $currentDir\cache\$category\$depend_resFolderName)) {
+                    Copy-Item -Path $currentDir\cache\$category\$depend_resFolderName $currentDir\platforms\android -Force -Recurse
                 }
                 
 
-                if (-not (Test-Path "$currentDir\platforms\android\$depend_artifactId-$depend_version-res\values")) {
-                    New-Item -Path $currentDir\platforms\android\$depend_artifactId-$depend_version-res\values -ItemType "directory"
-                    New-Item -Path $currentDir\platforms\android\$depend_artifactId-$depend_version-res\values\strings.xml
-                    Set-Content -Path $currentDir\platforms\android\$depend_artifactId-$depend_version-res\values\strings.xml -Value $defaultResource
+                if (-not (Test-Path "$currentDir\platforms\android\$depend_resFolderName\values")) {
+                    New-Item -Path $currentDir\platforms\android\$depend_resFolderName\values -ItemType "directory"
+                    New-Item -Path $currentDir\platforms\android\$depend_resFolderName\values\strings.xml
+                    Set-Content -Path $currentDir\platforms\android\$depend_resFolderName\values\strings.xml -Value $defaultResource
                 }
             }
 
-            $ADT_FILES += "$depend_artifactId-$depend_version.jar "
+            $ADT_FILES += "$depend_groupId-$depend_artifactId-$depend_version.jar "
             if ($depend_type -eq "aar") {
-                $ADT_FILES += "$depend_artifactId-$depend_version-res/. "
+                $ADT_FILES += "$depend_resFolderName/. "
             }
 
-            Copy-Item -Path $currentDir\cache\$category\$depend_artifactId-$depend_version.jar $currentDir\platforms\android\$depend_artifactId-$depend_version.jar -Force
+            Copy-Item -Path $currentDir\cache\$category\$depend_groupId-$depend_artifactId-$depend_version.jar $currentDir\platforms\android\$depend_groupId-$depend_artifactId-$depend_version.jar -Force
         }
     }
 
@@ -324,23 +364,21 @@ for($i=0;$i -lt $XmlDocument.packages.ChildNodes.Count;$i++) {
     Write-Host "Cleaning up" -ForegroundColor yellow
 
     if ($type -eq "aar") {
-        Remove-Item $currentDir\platforms\android\$artifactId-$version-res -Recurse
+        Remove-Item $currentDir\platforms\android\$resFolderName -Recurse
     }
-    Remove-Item $currentDir\platforms\android\$artifactId-$version.jar
+    Remove-Item $currentDir\platforms\android\$groupId-$artifactId-$version.jar
 
 
     if($numDependancies -gt 0) {
         $dependancies = $XmlDocument.packages.package[$i].dependancies
         for($j=0;$j -lt $numDependancies;$j++) {
-            #$depend_artifactId = $dependancies.package[$j].artifactId
-            #$depend_version = $dependancies.package[$j].version
-            #$depend_type = $dependancies.package[$j].type
-
             if($numDependancies -eq 1) {
+                $depend_groupId = $dependancies.package.groupId
                 $depend_artifactId = $dependancies.package.artifactId
                 $depend_version = $dependancies.package.version
                 $depend_type = $dependancies.package.type
             } else {
+                $depend_groupId = $dependancies.package[$j].groupId
                 $depend_artifactId = $dependancies.package[$j].artifactId
                 $depend_version = $dependancies.package[$j].version
                 $depend_type = $dependancies.package[$j].type
@@ -348,9 +386,9 @@ for($i=0;$i -lt $XmlDocument.packages.ChildNodes.Count;$i++) {
 
 
             if ($depend_type -eq "aar") {
-                Remove-Item $currentDir\platforms\android\$depend_artifactId-$depend_version-res -Recurse
+                Remove-Item $currentDir\platforms\android\$depend_groupId-$depend_artifactId-$depend_version-res -Recurse
             }
-            Remove-Item $currentDir\platforms\android\$depend_artifactId-$depend_version.jar
+            Remove-Item $currentDir\platforms\android\$depend_groupId-$depend_artifactId-$depend_version.jar
         }
     }
 
